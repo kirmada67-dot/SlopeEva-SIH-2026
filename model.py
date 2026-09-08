28b2349dd63dc75b0eae5bfc4ff18a9309396bb2
kirmada67-dot <kirmada67@gmail.com>
2026-09-08 22:16:12 +0530

    adding demo dataset

--- model.py
+++ model.py
@@ -3,4 +3,8 @@ import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
 
+df = pd.read_csv("src/landslide_demo_v1.csv")
+
+print(df.info())
