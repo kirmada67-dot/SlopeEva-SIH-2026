ecc70c60efc4c860d6273ec70325ce00e4c5e01b
kirmada67-dot <kirmada67@gmail.com>
2026-09-14 21:32:06 +0530

    code update

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import classification_report, confusion_matrix
 
 
-df = pn.read_csv("../src/landslide_demo_v1.csv")
+df = pn.read_csv("src/landslide_demo_v1.csv")
 x = df.drop("Landslide", axis=1)
 y = df["Landslide"]
 
@@ -15,7 +15,10 @@ train_x, test_x, train_y, test_y = train_test_split(x, y, test_size=0.2, random_
 model = RandomForestClassifier(n_estimators=100, random_state=42)
 model.fit(train_x, train_y)
 
-pred = model.predict(test_x)
+pred = model.predict_proba(test_x)[:, 1]
 
-print(classification_report(test_y, pred))
-print(confusion_matrix(test_y, pred))
+#print(classification_report(test_y, pred))
+#print(confusion_matrix(test_y, pred))
+
+#print(model.score(test_x, test_y))
+print(pred[:20])
